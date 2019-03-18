db230e5a96e4d9c15ed1a6aaadf3b9640b29c87f
soriojas <32626362+soriojas@users.noreply.github.com>
2019-03-18 14:33:19 -0400

    Created folder MCCStudyTool

---
+++ MCCStudyTool/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1552933768,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/9patch.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"style.css",
+		"akubin134.woff",
+		"akubin134.woff2",
+		"stylesheet.css"
+	]
+}
